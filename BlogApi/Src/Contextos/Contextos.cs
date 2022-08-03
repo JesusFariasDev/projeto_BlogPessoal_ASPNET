f6a90e5dd9d2e45b444daeb805dbb7944abc6a48
Jesus <je-farias@hotmail.com>
2022-08-03 11:34:18 -0300

    Criação de vínculos com SQL Server

---
+++ BlogApi/Src/Contextos/Contextos.cs
@@ -0,0 +1,26 @@
+﻿using BlogApi.Src.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Src.Contextos
+{
+    /// <summary>
+    /// <para>Resumo: Classe contexto, responsavel por carregar contexto e definir
+   /// DbSets</para>
+/// <para>Criado por: Generation</para>
+/// <para>Versão: 1.0</para>
+/// <para>Data: 17/07/2022</para>
+/// </summary>
+public class BlogPessoalContexto : DbContext
+    {
+        #region Atributos
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tema> Temas { get; set; }
+        public DbSet<Postagem> Postagens { get; set; }
+        #endregion
+        #region Construtores
+        public BlogPessoalContexto(DbContextOptions<BlogPessoalContexto> opt) : base(opt)
+        {
+        }
+        #endregion
+    }
+}
--- BlogApi/Startup.cs
+++ BlogApi/Startup.cs
@@ -1,6 +1,8 @@
+using BlogApi.Src.Contextos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,17 +26,25 @@ namespace BlogApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // Configuração de Banco de dados
+            services.AddDbContext<BlogPessoalContexto>(opt => opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
+            // Controladores
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BlogPessoalContexto contexto)
         {
+            //Ambiente de Desenvolvimento
             if (env.IsDevelopment())
             {
+                contexto.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
             }
+            // Ambiente de produção
+            contexto.Database.EnsureCreated();
+
 
             app.UseRouting();
 

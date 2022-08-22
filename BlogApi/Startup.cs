365a9d0627b98eee725ecc4c490357d79782b5ab
Jesus <je-farias@hotmail.com>
2022-08-22 15:44:39 -0300

    Ajustes task

--- BlogApi/Startup.cs
+++ BlogApi/Startup.cs
@@ -1,5 +1,6 @@
 using BlogApi.Src.Contextos;
 using BlogApi.Src.Repositorios;
+using BlogApi.Src.Repositorios.Implementacoes;
 using BlogApi.Src.Servicos;
 using BlogApi.Src.Servicos.Implementacoes;
 using BlogAPI.Src.Repositorios.Implentacoes;
@@ -13,9 +14,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,17 +37,31 @@ namespace BlogApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Configuração de Banco de dados
-            services.AddDbContext<BlogPessoalContexto>(opt => opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
+
+            // Configuraçãp Banco de Dados
+            if (Configuration["Enviroment:Start"] == "PROD")
+            {
+                services.AddEntityFrameworkNpgsql()
+                .AddDbContext<BlogPessoalContexto>(
+                opt =>
+                opt.UseNpgsql(Configuration["ConnectionStringsProd:DefaultConnection"]));
+            }
+            else
+            {
+                services.AddDbContext<BlogPessoalContexto>(
+                opt =>
+                opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
+            }
+
 
             //Um atalho que indica que IUsuario sempre irá trazer UsuarioRepositorio
             services.AddScoped<IUsuario, UsuarioRepositorio>();
+            services.AddScoped<IAutenticacao, AutenticacaoServicos>();
+            services.AddScoped<IPostagem, PostagemRepositorio>();
 
             // Controladores
             services.AddCors();//irá facilitar o uso em navegadores e ligação com o frontend
             services.AddControllers();
-            // Configuração de Serviços
-            services.AddScoped<IAutenticacao, AutenticacaoServicos>();
             // Configuração do Token Autenticação JWTBearer
             var chave = Encoding.ASCII.GetBytes(Configuration["Settings:Secret"]);
             services.AddAuthentication(a =>
@@ -63,6 +81,50 @@ namespace BlogApi
                 };
             }
             );
+            // Configuração Swagger
+            services.AddSwaggerGen(
+            s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Blog Pessoal",
+                    Version =
+                "v1"
+                });
+                s.AddSecurityDefinition(
+                "Bearer",
+                new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT authorization header utiliza: Bearer + JWT Token",
+                }
+                );
+                s.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new List<string>()
+                        }
+                    }
+                );
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+            }
+);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,6 +135,11 @@ namespace BlogApi
             {
                 contexto.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlogPessoal v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
             // Ambiente de produção
             contexto.Database.EnsureCreated();
